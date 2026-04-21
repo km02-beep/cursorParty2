c0602cb84eb21ee29fcd607657295e5648d16470
marinuspaul <marinuspaul@mailbox.org>
2026-04-21 23:55:45 +0200

    remove name generator

--- index.js
+++ index.js
@@ -19,69 +19,16 @@ app.get("/", (req, res) => {
   res.status(200).send("OK");
 });
 
-const adjectives = [
-  "Sleepy",
-  "Grumpy",
-  "Sneaky",
-  "Wobbly",
-  "Fluffy",
-  "Clumsy",
-  "Spooky",
-  "Hangry",
-  "Goofy",
-  "Jumpy",
-  "Dizzy",
-  "Whiny",
-  "Bouncy",
-  "Cranky",
-  "Sassy",
-  "Dopey",
-  "Noisy",
-  "Cheeky",
-  "Lazy",
-  "Hyper",
-];
-
-const nouns = [
-  "Potato",
-  "Noodle",
-  "Pickle",
-  "Waffle",
-  "Burrito",
-  "Muffin",
-  "Nugget",
-  "Pretzel",
-  "Biscuit",
-  "Pancake",
-  "Dumpling",
-  "Taco",
-  "Donut",
-  "Meatball",
-  "Cupcake",
-  "Banana",
-  "Avocado",
-  "Sausage",
-  "Crouton",
-  "Nacho",
-];
-
 // Harmonically pleasant frequencies (pentatonic scale across 3 octaves)
 const frequencies = [
   165, 185, 220, 247, 277, 330, 370, 415, 440, 494, 554, 587, 659, 740, 880,
 ];
 const backgroundTracks = ["bg1.mp3", "bg2.mp3", "bg3.mp3"];
 
-function generateName() {
-  const adj = adjectives[Math.floor(Math.random() * adjectives.length)];
-  const noun = nouns[Math.floor(Math.random() * nouns.length)];
-  return `${adj} ${noun}`;
-}
-
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
   const userColor = `#ffffff`;
-  const userName = generateName();
   const userFreq = frequencies[Math.floor(Math.random() * frequencies.length)];
   const userBackgroundTrack =
     backgroundTracks[Math.floor(Math.random() * backgroundTracks.length)];
@@ -89,7 +36,6 @@ io.on("connection", (socket) => {
   // Send this user their own identity
   socket.emit("welcome", {
     color: userColor,
-    name: userName,
     freq: userFreq,
     backgroundTrack: userBackgroundTrack,
   });
